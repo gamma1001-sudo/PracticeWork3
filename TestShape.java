787dce5cd0c889042b587ca5517ae30801a2584c
gamma1001-sudo <gamma.1001@yandex.ru>
2026-05-12 01:14:31 +0300

    Add files via upload

---
+++ TestShape.java
@@ -0,0 +1,23 @@
+public class TestShape {
+
+    public static void main(String[] args) {
+
+        Shape s1 = new Circle(5.5, "RED", false);
+
+        System.out.println(s1);
+        System.out.println("Area: " + s1.getArea());
+        System.out.println("Perimeter: " + s1.getPerimeter());
+
+        Shape s2 = new Rectangle(2.0, 4.0, "BLUE", true);
+
+        System.out.println(s2);
+        System.out.println("Area: " + s2.getArea());
+        System.out.println("Perimeter: " + s2.getPerimeter());
+
+        Shape s3 = new Square(6.0, "GREEN", true);
+
+        System.out.println(s3);
+        System.out.println("Area: " + s3.getArea());
+        System.out.println("Perimeter: " + s3.getPerimeter());
+    }
+}
